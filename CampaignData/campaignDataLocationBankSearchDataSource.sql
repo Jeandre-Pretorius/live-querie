a9c5107b42cf02ea9e009e598fde0fe7a82d6dc9
Jeandre Pretorius <jeandre@cubezoo.joburg>
2024-02-07 16:22:39 +0200

    added neccessary apostrophe

--- CampaignData/campaignDataLocationBankSearchDataSource.sql
+++ CampaignData/campaignDataLocationBankSearchDataSource.sql
@@ -1,4 +1,4 @@
-SELECT store_location AS geom, id, date, brand_id, brandName, locationId, locationName, latitude, longitude, metric, total, rating
+`SELECT store_location AS geom, id, date, brand_id, brandName, locationId, locationName, latitude, longitude, metric, total, rating
 FROM carto-389006.nandos.nandos_location_bank_search_data 
 WHERE store_location IS NOT NULL
-LIMIT 5000
+LIMIT 5000`
