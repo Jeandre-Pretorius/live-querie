73c48255c177d348c23d7147a1fe4a77703333f2
Jeandre Pretorius <jeandre@cubezoo.joburg>
2024-02-07 16:58:44 +0200

    basic SQL

--- CampaignData/campaignDataLocationBankSearchDataSource.sql
+++ CampaignData/campaignDataLocationBankSearchDataSource.sql
@@ -1,3 +1,2 @@
 SELECT store_location AS geom, id, date, brand_id, brandName, locationId, locationName, latitude, longitude, metric, total, rating
 FROM carto-389006.nandos.nandos_location_bank_search_data 
-WHERE store_location IS NOT NULL
