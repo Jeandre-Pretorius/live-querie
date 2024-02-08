90c955f2ea36b860cb6706bd7d768011df2177a6
Jeandre-Pretorius <148856725+Jeandre-Pretorius@users.noreply.github.com>
2024-02-08 09:46:38 +0200

    Update campaignDataLocationBankSearchDataSource.sql

--- CampaignData/campaignDataLocationBankSearchDataSource.sql
+++ CampaignData/campaignDataLocationBankSearchDataSource.sql
@@ -1,2 +1,3 @@
 SELECT store_location AS geom, id, date, brand_id, brandName, locationId, locationName, latitude, longitude, metric, total, rating
-FROM carto-389006.nandos.nandos_location_bank_search_data 
+FROM carto-389006.nandos.nandos_location_bank_search_data
+LIMIT 5000;
